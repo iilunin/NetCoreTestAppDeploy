ca0c506ca98a476b72bc05e00882c303fe6670f6
Igor Ilunin <ilunin.igor@gmail.com>
2018-11-06 22:19:12 -0500

    initial commit

---
+++ NetCoreTestApp/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreTestApp.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "API is working " + DateTime.Now.ToString();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return string.Format("value {0}", id);
+        }
+
+    }
+}
